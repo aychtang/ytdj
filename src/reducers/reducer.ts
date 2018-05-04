27a74a7eff4a0f36c66c499717f9d75ced637203
Howard Tang <howard@tray.io>
2018-05-04 20:50:02 +0100

    Reducer: Add remove turntable case.

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -23,21 +23,37 @@ const defaultState : T.AppState = {
 const turntableIdLens = (id: number) =>
     lensPath(["turntables", id]);
 
+// -----------------------------------------------------------------------------
 
 export default function AppReducer (
     state: T.AppState = defaultState,
     action: T.AppAction,
-) {
+) : T.AppState {
+    // Do nothing if no valid action found:
+    if (!action || !action.type) {
+        return state;
+    }
+
+    // Register a new turntable model at the current ID:
     if (action.type === "REGISTER_TURNTABLE") {
         const newState = set(
             turntableIdLens(currentId),
             createTurntable(currentId),
-            state
+            state,
         );
 
         currentId = currentId + 1;
 
         return newState;
+    }
+
+    // Remove the turntable model from the redux store:
+    if (action.type === "REMOVE_TURNTABLE") {
+        return set(
+            turntableIdLens(action.id),
+            null,
+            state,
+        );
     }
 
     return state;
