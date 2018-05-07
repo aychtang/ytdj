2f214bcea526a7c1a82804f22752237e77988002
Howard Tang <howard@tray.io>
2018-05-07 01:43:30 +0100

    Reducer: Check that reducer doesnt break if removing already removed turntable.

--- src/reducers/__tests__/reducer.ts
+++ src/reducers/__tests__/reducer.ts
@@ -9,7 +9,7 @@ describe("Application Reducer", () => {
 
     describe('REGISTER_TURNTABLE reduction', () => {
 
-        it("should add a new turntable to the redux store", () => {
+        it("should add a new turntable to the redux store.", () => {
             const state = AppReducer(undefined, undefined);
             expect(state.turntables[0]).toBeUndefined();
 
@@ -17,7 +17,7 @@ describe("Application Reducer", () => {
             expect(state2.turntables[0]).toBeDefined();
         });
 
-        it('should increment id values of new turntables by 1', () => {
+        it('should increment id values of new turntables by 1.', () => {
             const state = AppReducer(undefined, { type: REGISTER_TURNTABLE });
             expect(state.turntables[0].id).toBe(0);
 
@@ -32,7 +32,22 @@ describe("Application Reducer", () => {
 
     describe('REMOVE_TURNTABLE reduction', () => {
 
-        it('should remove a given turntable', () => {
+        it('should remove a given turntable.', () => {
+            const state = AppReducer(undefined, { type: REGISTER_TURNTABLE });
+            const state2 = AppReducer(state, { type: REMOVE_TURNTABLE, id: 0 });
+
+            expect(state2.turntables[0]).toBe(null);
+        });
+
+        it('should do nothing when removing a turntable thats already removed.', () => {
+            const state = AppReducer(undefined, { type: REGISTER_TURNTABLE });
+            const state2 = AppReducer(state, { type: REMOVE_TURNTABLE, id: 0 });
+            const state3 = AppReducer(state2, { type: REMOVE_TURNTABLE, id: 0 });
+
+            expect(state3.turntables[0]).toBe(null);
+        });
+
+        it('should remove a given turntable at any index.', () => {
             const state = AppReducer(undefined, { type: REGISTER_TURNTABLE });
             const state2 = AppReducer(state, { type: REGISTER_TURNTABLE });
             const state3 = AppReducer(state2, { type: REGISTER_TURNTABLE });
@@ -41,7 +56,7 @@ describe("Application Reducer", () => {
             expect(state4.turntables[1]).toBe(null);
         });
 
-        it('should not affect other turntables', () => {
+        it('should not affect other turntables.', () => {
             const state = AppReducer(undefined, { type: REGISTER_TURNTABLE });
             const state2 = AppReducer(state, { type: REGISTER_TURNTABLE });
             const state3 = AppReducer(state2, { type: REGISTER_TURNTABLE });
